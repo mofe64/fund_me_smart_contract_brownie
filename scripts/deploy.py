c4239bddd5d1a8b1abad0e58fe2b3eb355d667d4
Eyimofe Ogunbiyi <ogunbiyioladapo33@gmail.com>
2021-10-10 12:55:37 +0100

    project setup witj mocks

---
+++ scripts/deploy.py
@@ -0,0 +1,29 @@
+from brownie import FundMe, network, config, MockV3Aggregator
+from scripts.util import get_account
+
+
+def deploy_fund_me():
+    account = get_account()
+
+    if network.show_active() != "development":
+        price_feed_address = config["networks"][network.show_active()][
+            "eth_usd_price_feed"
+        ]
+    else:
+        print(f"The active network is {network.show_active()}")
+        print("Deploying mocks ...")
+        mock_aggregator = MockV3Aggregator.deploy(
+            18, 2000000000000000000000, {"from": account}
+        )
+        price_feed_address = mock_aggregator.address
+        print("Mock deployed")
+    fund_me = FundMe.deploy(
+        price_feed_address,
+        {"from": account},
+        publish_source=config["networks"][network.show_active()].get("verify"),
+    )
+    print(f"contract deployed to {fund_me.address}")
+
+
+def main():
+    deploy_fund_me()
